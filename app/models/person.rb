67896e249351205d27ef1d552131857b6844302f
EC2 Default User <ec2-user@ip-172-31-29-211.ap-southeast-1.compute.internal>
2019-04-09 10:49:31 +0000

    アソシエーションの追加

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ApplicationRecord
+    has_many :message
+    
     validates :name, presence: {message: 'は、必須項目です。'}
     validates :age, numericality: {message: 'は、数字で入力ください。'}
     validates :mail, email:{message: 'は、メールアドレスではありません。'}
